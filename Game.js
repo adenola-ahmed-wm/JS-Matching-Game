eb58bc90b12c8435a946da328acc0e84cff4d54a
Start Coding Student 2 <session2@START-Coding-MBP-09.local>
2016-08-16 18:10:15 -0700

    Added an image and changed up to reflect but it did not work

--- Game.js
+++ Game.js
@@ -5,18 +5,23 @@
 
 
 function generateFaces() {
+    //var theRightSide = document.getElementById("rightSide");
     var theLeftSide = document.getElementById("leftSide");
     var numberOfFaces = 5;
     for (var i = 0; i < numberOfFaces; i++) {
         var img = document.createElement('img');
-        img.src = "Superman_shield.svg.png";
+        img.src = "donut.png";
         img.height = 100;
         img.width = 100;
-
-        theLeftSide.appendChild(img).style.left = (Math.random() * 370) + "px";
         theLeftSide.appendChild(img).style.top = (Math.random() * 370) + "px";
+        theLeftSide.appendChild(img).style.left = (Math.random() * 370) + "px";
+            //theRightSide.appendChild(img).style.top = (Math.random() * 370) + "px";
+            //theRightSide.appendChild(img).style.left = (Math.random() * 370) + "px";
+
+
     }
 }
+
 
 
 
